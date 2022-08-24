9409ac5c40b25e268a4f1f7aab256a1a4e12ab44
zencode <zencode.id@gmail.com>
2022-08-24 11:01:32 +0700

    add addcontacts  button

--- src/components/ContactApp.js
+++ src/components/ContactApp.js
@@ -30,6 +30,8 @@ class ContactApp extends React.Component {
    }
  
    this.onDeleteHandler = this.onDeleteHandler.bind(this);
+   //penambahan handler addbooks
+   this.onAddContactHandler = this.onAddContactHandler.bind(this);
  }
  
  onDeleteHandler(id) {
@@ -37,14 +39,46 @@ class ContactApp extends React.Component {
    this.setState({ contacts });
  }
  
- render() {
-   return (
-     <div className="contact-app">
-       <h1>Daftar Kontak</h1>
-       <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
-     </div>
-   );
- }
+ //penmbahan /edit ke 2 ,penambahan fitur add contacts
+ onAddContactHandler({ name, tag }) {
+  this.setState((prevState) => {
+    return {
+      contacts: [
+        ...prevState.contacts,
+        {
+          id: +new Date(),
+          name,
+          tag,
+          imageUrl: '/images/default.jpg',
+        }
+      ]
+    }
+  });
 }
+
+//  render() {
+//    return (
+//      <div className="contact-app">
+//        <h1>Daftar Kontak</h1>
+//        <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
+//      </div>
+//    );
+//  }
+// }
  
+// export default ContactApp;
+
+render() {
+  return (
+    <div className="contact-app">
+      <h1>Aplikasi Kontak</h1>
+      <h2>Tambah Kontak</h2>
+      <ContactInput addContact={this.onAddContactHandler} />
+      <h2>Daftar Kontak</h2>
+      <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
+    </div>
+  );
+}
+}
+
 export default ContactApp;
