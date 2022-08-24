f1419ae60c3292638df1665cb7eb0f570c8a7ffe
zencode <zencode.id@gmail.com>
2022-08-24 09:35:44 +0700

    add delete button

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,16 +1,40 @@
+// 1.kode sebelum dirubah
+
+// import React from 'react';
+// import ContactItem from './ContactItem';
+ 
+// function ContactList({ contacts }) {
+//  return (
+//    <div className="contact-list">
+//      {
+//        contacts.map((contact) => (
+//          <ContactItem key={contact.id} {...contact} />
+//        ))
+//      }
+//    </div>
+//  );
+// }
+ 
+// export default ContactList;
+
+// 2.penambahan kode handle onDelete
 import React from 'react';
 import ContactItem from './ContactItem';
  
-function ContactList({ contacts }) {
- return (
-   <div className="contact-list">
-     {
-       contacts.map((contact) => (
-         <ContactItem key={contact.id} {...contact} />
-       ))
-     }
-   </div>
- );
+function ContactList({ contacts, onDelete }) {
+  return (
+    <div className="contact-list">
+      {
+        contacts.map((contact) => (
+          <ContactItem 
+          key={contact.id}
+          id={contact.id}
+          onDelete={onDelete}
+          {...contact} />
+        ))
+      }
+    </div>
+  );
 }
  
 export default ContactList;
